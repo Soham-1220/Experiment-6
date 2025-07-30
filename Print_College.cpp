1507a5569db49f6ec9b3ed12f901055f6282dca8
Soham <sohambhogaonkar20@gmail.com>
2025-07-31 01:10:52 +0530

    Print_College.cpp

---
+++ Print_College.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main (){
+    for (int i=0;i<5;i++){
+       
+            cout<<i+1<<"."<< "COLLEGE"<<endl;
+       
+    }
+  return 0;
+}
+output
+1.COLLEGE
+2.COLLEGE
+3.COLLEGE
+4.COLLEGE
+5.COLLEGE 
